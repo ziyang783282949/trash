2e398f76ea254697c92dcc52c84990b602e276f5
zy <783282949@qq.com>
2017-07-09 11:52:28 +0800

    location

--- myapplication/app/src/main/java/com/zhongying/zy/sharetrash/activity/Location.java
+++ myapplication/app/src/main/java/com/zhongying/zy/sharetrash/activity/Location.java
@@ -1,10 +1,8 @@
 package com.zhongying.zy.sharetrash.activity;
 
 
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,46 +12,167 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.DPoint;
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.CoordinateConverter;
 import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.UiSettings;
-import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.LatLng;
-import com.amap.api.maps2d.model.Marker;
 import com.amap.api.maps2d.model.MarkerOptions;
 import com.amap.api.maps2d.model.MyLocationStyle;
-import com.amap.api.maps2d.model.PolylineOptions;
 import com.zhongying.zy.sharetrash.R;
+import com.zhongying.zy.sharetrash.UserService.Utils.CoordinateUtil;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Location extends AppCompatActivity {
-        //显示地图需要的变量
-        private MapView mapView;//地图控件
-        private AMap aMap;//地图对象
+public class Location extends AppCompatActivity implements LocationSource,AMapLocationListener{
+    //显示地图需要的变量
+    private MapView mapView;//地图控件
+    private AMap aMap;//地图对象
+    private Context mContext=this;
+    /** 声明mlocationClient对象 */
+    private AMapLocationClient mlocationClient = null;
+
+    /** 声明mLocationOption对象 */
+    private AMapLocationClientOption mLocationOption = null;
+    /** 定位监听器 */
+    private OnLocationChangedListener mListener = null;
+
+    //标识，用于判断是否只显示一次定位信息和用户重新定位
+    private boolean isFirstLoc = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-        mapView= (MapView) findViewById(R.id.map);
+        mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
-        aMap=mapView.getMap();
-
+        aMap = mapView.getMap();
         addMarkerToMap();
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        aMap.setMyLocationStyle(myLocationStyle);
+        initLocation();
+    }
+
+    private void addMarkerToMap() {
+        MarkerOptions markerOption1 = new MarkerOptions();
+        markerOption1.position(new LatLng(39.9081728469, 116.3867845961));
+        markerOption1.draggable(true);
+
+        MarkerOptions markerOption2 = new MarkerOptions();
+        markerOption2.position(new LatLng(40.9081728469, 116.4867845961));
+        markerOption2.draggable(true);
+
+        aMap.addMarker(markerOption1);
+        aMap.addMarker(markerOption2);
 
     }
-    private void addMarkerToMap() {
-        LatLng latLng = new LatLng(39.9081728469, 116.3867845961);
-        MarkerOptions markerOption = new MarkerOptions();
-        markerOption.position(latLng);
-        markerOption.draggable(true);
-        markerOption.icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker));
-        Marker marker = aMap.addMarker(markerOption);
-        marker.setRotateAngle(30);
+
+    /**
+     * 获取定位坐标
+     */
+    public void initLocation() {
+
+        //设置显示定位按钮 并且可以点击
+        UiSettings settings = aMap.getUiSettings();
+        aMap.setLocationSource(this);//设置了定位的监听,这里要实现LocationSource接口
+        // 是否显示定位按钮
+        settings.setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
+        mlocationClient = new AMapLocationClient(this);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位监听
+        mlocationClient.setLocationListener(this);
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //设置是否只定位一次,默认为false
+        mLocationOption.setOnceLocation(false);
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。
+        //如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会。
+        //mLocationOption.setOnceLocationLatest(true);
+        //设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        //启动定位
+        mlocationClient.startLocation();
+    }
+    public AMapLocation fromGpsToAmap(AMapLocation location) {
+        LatLng latLng =null;
+        latLng = CoordinateUtil.transform(location.getLatitude(), location.getLongitude());
+        AMapLocation aMapLocation = new AMapLocation(location);
+        aMapLocation.setLatitude(latLng.latitude);
+        aMapLocation.setLongitude(latLng.longitude);
+
+        return aMapLocation;
+    }
+    /**
+     * 高德定位回调
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                if (isFirstLoc) {
+                    //设置缩放级别
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+                    //将地图移动到定位点
+                    aMapLocation=fromGpsToAmap(aMapLocation);
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                    //点击定位按钮 能够将地图的中心移动到定位点
+                    mListener.onLocationChanged(aMapLocation);
+                    Toast.makeText(getApplicationContext(), "定位成功", Toast.LENGTH_SHORT).show();
+                    isFirstLoc = false;
+                }
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mlocationClient.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener=onLocationChangedListener;
+    }
+
+    @Override
+    public void deactivate() {
+        mListener=null;
     }
 }
 
